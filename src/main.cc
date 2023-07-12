64b55087336cc22347925f03449cdb7f5d9e48d6
Konstantin Lübeck <konstantin.luebeck@uni-tuebingen.de>
2023-07-12 13:41:41 +0200

    Unordered map with pair as key works.

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory>
+#include <set>
 #include <unordered_map>
 
 class Animal {
@@ -42,22 +43,6 @@ public:
     virtual void make_noise() { std::cout << "ruff" << std::endl; }
 };
 
-class MapKey {
-public:
-    MapKey(std::shared_ptr<Animal> first, std::shared_ptr<Animal> second)
-        : first(first), second(second){};
-    std::shared_ptr<Animal> first;
-    std::shared_ptr<Animal> second;
-
-    bool operator==(const MapKey& map_key) const {
-        // std::cout << "call MapKey.operator==" << std::endl;
-        auto x0 = map_key.first->name.compare(first->name) == 0;
-        auto x1 = map_key.second->name.compare(second->name) == 0;
-
-        return x0 & x1;
-    }
-};
-
 template <>
 struct std::hash<Animal> {
     std::size_t operator()(Animal const& a) const {
@@ -74,49 +59,22 @@ struct std::hash<std::shared_ptr<Animal>> {
     }
 };
 
-template <>
-struct std::hash<MapKey> {
-    std::size_t operator()(MapKey const& mk) const {
-        auto h0 = std::hash<std::shared_ptr<Animal>>{}(mk.first);
-        auto h1 = std::hash<std::shared_ptr<Animal>>{}(mk.second);
-
-        return h0 ^ h1;
-    }
-};
-
-struct Deref {
-    struct Hash {
-        template <typename T>
-        std::size_t operator()(const std::shared_ptr<T>& p) const {
-            // std::cout << "call: Deref" << std::endl;
-            return std::hash<T>()(*p);
-        }
-    };
-    struct Compare {
-        template <typename T>
-        std::size_t operator()(const std::shared_ptr<T>& a,
-                               const std::shared_ptr<T>& b) const {
-            // std::cout << "call: Compare" << std::endl;
-            return *a == *b;
-        }
-    };
-};
-
-struct PairHash {
-    template <class T1, class T2>
-    std::size_t operator()(const std::pair<T1, T2>& p) const {
-        auto h0 = std::hash<T1>{}(p.first);
-        auto h1 = std::hash<T1>{}(p.second);
-
-        // std::cout << "call: PairHash, " << h0 << "^" << h1 << " = " << (h0^h1) <<
-        // std::endl;
-
-        return h0 ^ h1;
-    }
-};
-
 int main(int argc, char** argv) {
-    auto m = std::unordered_map<MapKey, int>();
+    using key_type = std::pair<std::shared_ptr<Animal>, std::shared_ptr<Animal>>;
+
+    auto equal = [](const key_type& a, const key_type& b) {
+        return (a.first->name.compare(b.first->name) == 0) &&
+               (a.second->name.compare(b.second->name) == 0);
+    };
+
+    auto hash = [](const key_type& p) {
+        auto h0 = std::hash<std::shared_ptr<Animal>>{}(p.first);
+        auto h1 = std::hash<std::shared_ptr<Animal>>{}(p.second);
+        return h0 ^ h1;
+    };
+
+    std::unordered_map<key_type, int, decltype(hash), decltype(equal)> m(8, hash,
+                                                                         equal);
 
     auto a0 = std::make_shared<Animal>("a");
     auto a1 = std::make_shared<Animal>("a");
@@ -124,107 +82,15 @@ int main(int argc, char** argv) {
     auto b0 = std::make_shared<Animal>("b");
     auto b1 = std::make_shared<Animal>("b");
 
-    auto key0 = MapKey(a0, b0);
-    auto key1 = MapKey(a0, b0);
+    m.insert({key_type(a0, b0), 42});
 
-    std::cout << (key0 == key1) << std::endl;
-
-    m.insert(std::make_pair(key0, 23));
-
-    if (m.find(key0) != m.end()) {
-        std::cout << "> found key0: " << m.at(key0) << std::endl;
+    if (m.find(key_type(a0, b0)) != m.end()) {
+        std::cout << "> found a0,b0: " << m.at(key_type(a0, b0)) << std::endl;
     }
 
-    if (m.find(key1) != m.end()) {
-        std::cout << "> found key1: " << m.at(key1) << std::endl;
+    if (m.find(key_type(a1, b1)) != m.end()) {
+        std::cout << "> found a1,b1: " << m.at(key_type(a1, b1)) << std::endl;
     }
-
-    m.insert(std::make_pair(key1, 42));
-
-    if (m.find(key1) != m.end()) {
-        std::cout << "> found key1: " << m.at(key1) << std::endl;
-    }
-
-    if (m.find(key0) != m.end()) {
-        std::cout << "> found key0: " << m.at(key0) << std::endl;
-    }
-
-    /*
-    auto m =
-        std::unordered_map<std::pair<std::shared_ptr<Animal>, std::shared_ptr<Animal>>,
-                           int, PairHash>();
-
-    auto a0 = std::make_shared<Animal>("a");
-    auto a1 = std::make_shared<Animal>("a");
-
-    auto b0 = std::make_shared<Animal>("b");
-    auto b1 = std::make_shared<Animal>("b");
-
-    //std::cout << (*a0 == *a1) << std::endl;
-
-    auto hc0 = std::make_shared<HouseCat>("hc");
-    auto hc1 = std::make_shared<HouseCat>("hc");
-
-    //std::cout << (*hc0 == *hc1) << std::endl;
-
-    auto key0 = std::make_pair(a0, b0);
-    auto key1 = std::make_pair(a1, b1);
-
-    m.insert({key0, 42});
-
-    std::cout << "find key0" << std::endl;
-    if(m.find(key0) != m.end()) {
-        std::cout << "> found key0: " << m.at(key0) <<  std::endl;
-    }
-
-    std::cout << "find key1" << std::endl;
-    if(m.find(key1) != m.end()) {
-        std::cout << "> found key1: " << m.at(key1) <<  std::endl;
-    }
-
-    m.insert({key1, 23});
-
-    if(m.find(key1) != m.end()) {
-        std::cout << "> found key1: " << m.at(key1) <<  std::endl;
-    }
-
-    std::cout << "> found key0: " << m.at(key0) <<  std::endl;
-    /*
-    */
-
-    // std::cout << std::hash<Animal>{}(*a0) << std::endl;
-    // std::cout << std::hash<Animal>{}(*a1) << std::endl;
-    //
-    // std::cout << std::hash<std::shared_ptr<Animal>>{}(a0) << std::endl;
-    // std::cout << std::hash<std::shared_ptr<Animal>>{}(a1) << std::endl;
-
-    /*
-    auto fritz0 = std::make_shared<HouseCat>("Fritz");
-    auto bello0 = std::make_shared<Shepard>("Bello");
-
-    auto key0 = std::make_pair(fritz0, bello0);
-
-    m.insert(std::make_pair(key0, 1));
-
-    auto key1 = std::make_pair(fritz0, bello0);
-
-    auto fritz1 = std::make_shared<HouseCat>("Fritz");
-    auto bello1 = std::make_shared<HouseCat>("Bello");
-
-    auto key2 = std::make_pair(fritz1, bello1);
-
-    if (m.find(key0) != m.end()) {
-        std::cout << "key0: " << m.at(key0) << std::endl;
-    }
-
-    if (m.find(key1) != m.end()) {
-        std::cout << "key1: " << m.at(key1) << std::endl;
-    }
-
-    if (m.find(key2) != m.end()) {
-        std::cout << "key2: " << m.at(key1) << std::endl;
-    }
-    */
 
     return 0;
 }
